9e0a504bae31a7e7f466077b87d88323dc54f8e9
David <david@davidterra.com>
2018-06-05 21:41:48 -0300

    aula 46

---
+++ fundamentos/logicos.js
@@ -0,0 +1,14 @@
+function comprar(trabalho1, trabalho2) {
+    const comprarSorte = trabalho1 || trabalho2
+    const comprarTv50 = trabalho1 && trabalho2
+    // const comprarTv32 = !!(trabalho1 ^ trabalho2) // bitwise xor
+    const comprarTv32 = trabalho1 != trabalho2
+    const manterSaudavel = !comprarSorte // operador unário
+
+    return { comprarSorte, comprarTv50, comprarTv32, manterSaudavel }
+}
+
+console.log(comprar(true, true))
+console.log(comprar(true, false))
+console.log(comprar(false, true))
+console.log(comprar(false, false))

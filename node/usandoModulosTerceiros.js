aaa18462acee8241af2104f98b52cdaf1ae5ae93
David Terra Alves <d.terra@cigamgestor.com.br>
2018-07-12 13:56:13 -0300

    aula 121

---
+++ node/usandoModulosTerceiros.js
@@ -0,0 +1,13 @@
+const _ = require('lodash')
+setInterval(()=> console.log(_.random(1,1000)), 2000)
+
+/*
+    npm -v
+    npm i lodash
+    npm i -g nodemon 
+
+    cd node
+    nodemon usandoModulosTerceiros.js
+
+    rs (comando restart nodemon)
+*/
